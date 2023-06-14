a94004a81c09dff0728d5c15b33cc195264518a9
hyelliton <hyelliton@hotmail.com>
2023-06-13 21:49:11 -0300

    style tela AtivDiarias

---
+++ Telas/AtivDiarias/style.js
@@ -0,0 +1,51 @@
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    
+    checkboxContainer: {
+      flexDirection: 'row',
+      marginBottom: 20,
+    },
+  
+    checkbox: {
+      alignSelf: 'center',
+      marginTop:-25,
+      margin: 5,
+      marginBottom: 10,
+      borderRadius: 100
+      
+    },
+  
+    checkbox2: {
+      alignSelf: 'center',
+      marginLeft:-156,
+      marginTop:-435,
+      margin: 10,
+      marginBottom: 20
+    },
+    checkbox3: {
+      alignSelf: 'center',
+      marginLeft:-156,
+      marginTop:-345,
+      margin: 10,
+    },
+    
+    checkbox4: {
+      alignSelf: 'center',
+      marginLeft:-156,
+      marginTop:-250,
+      margin: 10,
+    },
+    
+    bottomVoltar:{
+      marginLeft: '50%',
+      marginEnd: '50%',
+      backgroundColor: 'red',
+      
+    },
+  
+    
+  });
